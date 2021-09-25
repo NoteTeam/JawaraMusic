201de444ac4aa28a686c4ba636f8cc33fede4190
NoteTeam <90462419+NoteTeam@users.noreply.github.com>
2021-09-25 23:33:07 +0700

    Update callback.py

--- handlers/callback.py
+++ handlers/callback.py
@@ -39,10 +39,10 @@ async def cbstart(_, query: CallbackQuery):
                         "📣 Official Channel", url=f"https://t.me/{UPDATES_CHANNEL}")
                 ],[
                     InlineKeyboardButton(
-                        "🌐 Wiki's Page", url="https://github.com/NoteTeam/selebboss/wiki/selebboss-Music-Wiki's")
+                        "🌐 Wiki's Page", url="https://github.com/NoteTeam/JawaraMusic/wiki/JawaraMusic-Music-Wiki's")
                 ],[
                     InlineKeyboardButton(
-                        "🧪 Source Code 🧪", url="https://github.com/NoteTeam/selebboss"
+                        "🧪 Source Code 🧪", url="https://github.com/NoteTeam/JawaraMusic"
                     )
                 ]
             ]
